f2066f4ff26dc4b0a5e4036a09ccd9b9a8dafda5
cyberck <dushaev2017@gmail.com>
2021-11-08 04:41:29 -0700

    cinema

---
+++ script.js
@@ -0,0 +1,106 @@
+// Циклы
+
+let imya = prompt("Vvedite imya");
+let god = +prompt("Vvedite god");
+let nineshniygod = +prompt("Vvedite nineshniy god");
+
+
+
+
+
+
+
+// let perviy = +prompt('Vvedite sifru');
+// let vtoroy = +prompt('Vvedite sifru stepen');
+
+// let answer = 1;
+// for(let i = 0; i < vtoroy;i++) {
+// answer = answer * vtoroy;
+
+// }
+// console.log(answer);
+
+
+
+// let com = '';
+// do {
+//     userNum = +prompt('Kolichestvo strok');
+// }while(isNaN(userNum) || userNum == 0) {
+//     com = userNum;
+// }
+
+// let symbol = prompt('Simvol otstupov ');
+// let finalSymbol = prompt('Konechniy simvol ');
+
+// let row = '';
+ 
+// for(let i = 0; i < 10; i++) {
+//     row += symbol;
+    
+//     console.log(row + finalSymbol);
+// }
+
+
+
+
+// let apple1 = '2';
+// let apple2 = '5';
+
+// alert(+apple1 + +apple2);
+
+
+
+
+// let level0 = prompt('Vvedite chto xotite');
+// console.log(level0);
+
+// let level1 = prompt('Vvedite chto xotite');
+// console.log(level1);
+
+// let level2 = prompt('Vvedite chto xotite');
+// console.log(level2);
+
+// let level3 = prompt('Vvedite chto xotite');
+// console.log(level3);
+
+// let level4 = prompt('Vvedite chto xotite');
+// console.log(level4);
+
+
+
+
+
+
+
+
+    // ******************
+// let num = +prompt('Vvedite sifru');
+// let num1 = +prompt('Vvedite sifru');
+
+// for(let i = 0; ) {
+
+// }
+// let admin, name;
+// name = 'Djon';         
+
+// admin = name;
+
+
+// alert(admin);
+
+    // ****************
+
+// let age = prompt('Skolko tebe let', 100);
+
+// alert(`Tebe ${age} let`);
+
+// let name = prompt('Kak vas zovut');
+
+// alert(`${name}`);
+
+        // ******
+
+// let sifra = +prompt('Vvedite sifru');
+// let stepen = +prompt('Vdeti stepen');
+
+// alert(`${sifra ** stepen}`)
